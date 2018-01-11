20de781e9201a1eec0d2614d131f4a66b18b784f
devwarrior <pi.glynn.farrow@gmail.com>
2018-01-11 23:21:40 +0800

    Amount: BTC Stringer units -> XZC

--- rpcclient/wallet.go
+++ rpcclient/wallet.go
@@ -432,7 +432,7 @@ func (r FutureSetTxFeeResult) Receive() error {
 //
 // See SetTxFee for the blocking version and more details.
 func (c *Client) SetTxFeeAsync(fee xzcutil.Amount) FutureSetTxFeeResult {
-	cmd := btcjson.NewSetTxFeeCmd(fee.ToBTC())
+	cmd := btcjson.NewSetTxFeeCmd(fee.ToXZC())
 	return c.sendCmd(cmd)
 }
 
@@ -471,7 +471,7 @@ func (r FutureSendToAddressResult) Receive() (*chainhash.Hash, error) {
 // See SendToAddress for the blocking version and more details.
 func (c *Client) SendToAddressAsync(address xzcutil.Address, amount xzcutil.Amount) FutureSendToAddressResult {
 	addr := address.EncodeAddress()
-	cmd := btcjson.NewSendToAddressCmd(addr, amount.ToBTC(), nil, nil)
+	cmd := btcjson.NewSendToAddressCmd(addr, amount.ToXZC(), nil, nil)
 	return c.sendCmd(cmd)
 }
 
@@ -497,7 +497,7 @@ func (c *Client) SendToAddressCommentAsync(address xzcutil.Address,
 	commentTo string) FutureSendToAddressResult {
 
 	addr := address.EncodeAddress()
-	cmd := btcjson.NewSendToAddressCmd(addr, amount.ToBTC(), &comment,
+	cmd := btcjson.NewSendToAddressCmd(addr, amount.ToXZC(), &comment,
 		&commentTo)
 	return c.sendCmd(cmd)
 }
@@ -550,7 +550,7 @@ func (r FutureSendFromResult) Receive() (*chainhash.Hash, error) {
 // See SendFrom for the blocking version and more details.
 func (c *Client) SendFromAsync(fromAccount string, toAddress xzcutil.Address, amount xzcutil.Amount) FutureSendFromResult {
 	addr := toAddress.EncodeAddress()
-	cmd := btcjson.NewSendFromCmd(fromAccount, addr, amount.ToBTC(), nil,
+	cmd := btcjson.NewSendFromCmd(fromAccount, addr, amount.ToXZC(), nil,
 		nil, nil)
 	return c.sendCmd(cmd)
 }
@@ -574,7 +574,7 @@ func (c *Client) SendFrom(fromAccount string, toAddress xzcutil.Address, amount 
 // See SendFromMinConf for the blocking version and more details.
 func (c *Client) SendFromMinConfAsync(fromAccount string, toAddress xzcutil.Address, amount xzcutil.Amount, minConfirms int) FutureSendFromResult {
 	addr := toAddress.EncodeAddress()
-	cmd := btcjson.NewSendFromCmd(fromAccount, addr, amount.ToBTC(),
+	cmd := btcjson.NewSendFromCmd(fromAccount, addr, amount.ToXZC(),
 		&minConfirms, nil, nil)
 	return c.sendCmd(cmd)
 }
@@ -603,7 +603,7 @@ func (c *Client) SendFromCommentAsync(fromAccount string,
 	comment, commentTo string) FutureSendFromResult {
 
 	addr := toAddress.EncodeAddress()
-	cmd := btcjson.NewSendFromCmd(fromAccount, addr, amount.ToBTC(),
+	cmd := btcjson.NewSendFromCmd(fromAccount, addr, amount.ToXZC(),
 		&minConfirms, &comment, &commentTo)
 	return c.sendCmd(cmd)
 }
@@ -659,7 +659,7 @@ func (r FutureSendManyResult) Receive() (*chainhash.Hash, error) {
 func (c *Client) SendManyAsync(fromAccount string, amounts map[xzcutil.Address]xzcutil.Amount) FutureSendManyResult {
 	convertedAmounts := make(map[string]float64, len(amounts))
 	for addr, amount := range amounts {
-		convertedAmounts[addr.EncodeAddress()] = amount.ToBTC()
+		convertedAmounts[addr.EncodeAddress()] = amount.ToXZC()
 	}
 	cmd := btcjson.NewSendManyCmd(fromAccount, convertedAmounts, nil, nil)
 	return c.sendCmd(cmd)
@@ -688,7 +688,7 @@ func (c *Client) SendManyMinConfAsync(fromAccount string,
 
 	convertedAmounts := make(map[string]float64, len(amounts))
 	for addr, amount := range amounts {
-		convertedAmounts[addr.EncodeAddress()] = amount.ToBTC()
+		convertedAmounts[addr.EncodeAddress()] = amount.ToXZC()
 	}
 	cmd := btcjson.NewSendManyCmd(fromAccount, convertedAmounts,
 		&minConfirms, nil)
@@ -722,7 +722,7 @@ func (c *Client) SendManyCommentAsync(fromAccount string,
 
 	convertedAmounts := make(map[string]float64, len(amounts))
 	for addr, amount := range amounts {
-		convertedAmounts[addr.EncodeAddress()] = amount.ToBTC()
+		convertedAmounts[addr.EncodeAddress()] = amount.ToXZC()
 	}
 	cmd := btcjson.NewSendManyCmd(fromAccount, convertedAmounts,
 		&minConfirms, &comment)
@@ -1123,7 +1123,7 @@ func (r FutureMoveResult) Receive() (bool, error) {
 //
 // See Move for the blocking version and more details.
 func (c *Client) MoveAsync(fromAccount, toAccount string, amount xzcutil.Amount) FutureMoveResult {
-	cmd := btcjson.NewMoveCmd(fromAccount, toAccount, amount.ToBTC(), nil,
+	cmd := btcjson.NewMoveCmd(fromAccount, toAccount, amount.ToXZC(), nil,
 		nil)
 	return c.sendCmd(cmd)
 }
@@ -1144,7 +1144,7 @@ func (c *Client) Move(fromAccount, toAccount string, amount xzcutil.Amount) (boo
 func (c *Client) MoveMinConfAsync(fromAccount, toAccount string,
 	amount xzcutil.Amount, minConfirms int) FutureMoveResult {
 
-	cmd := btcjson.NewMoveCmd(fromAccount, toAccount, amount.ToBTC(),
+	cmd := btcjson.NewMoveCmd(fromAccount, toAccount, amount.ToXZC(),
 		&minConfirms, nil)
 	return c.sendCmd(cmd)
 }
@@ -1167,7 +1167,7 @@ func (c *Client) MoveMinConf(fromAccount, toAccount string, amount xzcutil.Amoun
 func (c *Client) MoveCommentAsync(fromAccount, toAccount string,
 	amount xzcutil.Amount, minConfirms int, comment string) FutureMoveResult {
 
-	cmd := btcjson.NewMoveCmd(fromAccount, toAccount, amount.ToBTC(),
+	cmd := btcjson.NewMoveCmd(fromAccount, toAccount, amount.ToXZC(),
 		&minConfirms, &comment)
 	return c.sendCmd(cmd)
 }
