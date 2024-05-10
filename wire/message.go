c4175e1d2843de0a9185dda97412f1ff060b4490
seopub <142002838+seopub@users.noreply.github.com>
2024-05-10 10:15:01 +0300

    Update message.go

--- wire/message.go
+++ wire/message.go
@@ -10,7 +10,7 @@ import (
 	"io"
 	"unicode/utf8"
 
-	"github.com/zcoinofficial/xzcd/chaincfg/chainhash"
+	"github.com/seopub/xzcd/chaincfg/chainhash"
 )
 
 // MessageHeaderSize is the number of bytes in a bitcoin message header.
