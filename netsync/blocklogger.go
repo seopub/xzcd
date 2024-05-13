6ca0176880c8a81b54dfb8673ec4e9fd9d4b427d
seopub <142002838+seopub@users.noreply.github.com>
2024-05-13 10:50:05 +0300

    Update blocklogger.go

--- netsync/blocklogger.go
+++ netsync/blocklogger.go
@@ -9,7 +9,7 @@ import (
 	"time"
 
 	"github.com/seopub/btcsuite_btclog"
-	xzcutil "github.com/btcsuite/xzcutil"
+	"github.com/seopub/xzcutil"
 )
 
 // blockProgressLogger provides periodic logging for other services in order
